08982d1c164e4afe004868fa6893185818e6e909
Michael Williams <michael.williams@enspiral.com>
2016-04-10 18:06:42 +1200

    assign dom events passed into widget to dom element

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var Tc = require('tcomb')
 var pixelsToCanvas = require('pixels-canvas')
 var Ndpixels = require('ndpixels')
+var domEvents = require('@f/dom-events')
 
 // widget docs
 // https://github.com/Matt-Esch/virtual-dom/blob/master/docs/widget.md
@@ -15,6 +16,7 @@ function CanvasWidget (options) {
   Tc.assert(!Tc.Nil.is(options.pixels), 'options.pixels should be defined')
   this.pixels = Ndpixels(options.pixels)
   this.pixels = options.pixels
+  assignDomEvents(this, options)
 }
 
 CanvasWidget.prototype.type = "Widget"
@@ -22,10 +24,21 @@ CanvasWidget.prototype.init = function () {
   var canvas = document.createElement('canvas')
   this.render = pixelsToCanvas(canvas)
   this.render(this.pixels)
+  assignDomEvents(canvas, this)
   return canvas
 }
 CanvasWidget.prototype.update = function (previous, domNode) {
   this.render = this.render || previous.render
   this.render(this.pixels)
+  assignDomEvents(domNode, this)
 }
 CanvasWidget.prototype.destroy = function (domNode) {}
+
+function assignDomEvents (target, source) {
+  for (var i = 0; i < domEvents.length; ++i) {
+    var eventName = 'on' + domEvents[i]
+    if (source[eventName] && target[eventName] == null) {
+      target[eventName] = source[eventName]
+    }
+  }
+}
